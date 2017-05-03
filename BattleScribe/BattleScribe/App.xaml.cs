18a25a8441971b6a114e6bb0396dcf2ccb41e882
Jack // Casper Vissers <C.vissers97@outlook.com>
2017-05-03 10:41:24 +0200

    Folder structure updated, program base
    GetDescription();

--- BattleScribe/BattleScribe/App.xaml.cs
+++ BattleScribe/BattleScribe/App.xaml.cs
@@ -13,5 +13,6 @@ namespace BattleScribe
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ BattleScribe/BattleScribe/Classes/Items/Item.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleScribe.Classes.Items
+{
+    class Item
+    {
+        protected string name;
+        protected string description;
+        protected string type;
+        protected bool proficient;
+        protected bool attuneable;
+        protected double weight;
+
+        public Item()
+        {
+
+        }
+
+        public Item(string name, string description, string type, bool proficient, bool attuneable, double weight)
+        {
+            this.name = name;
+            this.description = description;
+            this.type = type;
+            this.proficient = proficient;
+            this.attuneable = proficient;
+            this.weight = weight;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetDescription()
+        {
+            return this.description;
+        }
+
+        public void SetDescription(string description)
+        {
+            this.description = description;
+        }
+
+        public string GetItemType()
+        {
+            return this.type;
+        }
+
+        public void SetItemType(string type)
+        {
+            this.type = type;
+        }
+
+        public bool GetProficient()
+        {
+            return this.proficient;
+        }
+
+        public void SetProficient(bool proficient)
+        {
+            this.proficient = proficient;
+        }
+
+        public bool GetAttunement()
+        {
+            return this.attuneable;
+        }
+
+        public void SetAttunement(bool attuneable)
+        {
+            this.attuneable = attuneable;
+        }
+
+        public double GetWeight()
+        {
+            return this.weight;
+        }
+
+        public void SetWeight(double weight)
+        {
+            this.weight = weight;
+        }
+    }
+}
