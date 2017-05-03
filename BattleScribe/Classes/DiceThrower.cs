b79c2faffc209bd86e6881dfff9997dfb8a045a8
Justnuked <thommartens14@hotmail.com>
2017-05-03 14:25:13 +0200

    Added diceroller and spell class

---
+++ BattleScribe/Classes/DiceThrower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleScribe.Classes
+{
+   static class DiceThrower
+    {
+       public static int ThrowDice(int amount, int sides, int mod)
+       {
+           int result = 0;
+           Random r = new Random();
+
+           for (int i = 0; i <= amount; i++)
+           {
+               result += r.Next(0, sides);
+           }
+
+           return result;
+       }
+    }
+}
---
+++ BattleScribe/Classes/Spell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleScribe.Classes
+{
+    public class Spell
+    {
+        private string name;
+        private byte level;
+        private string school;
+        private string components;
+        private string desc;
+        private string duration;
+        private string atHigherLevels;
+
+        public Spell()
+        { 
+        }
+    }
+}
