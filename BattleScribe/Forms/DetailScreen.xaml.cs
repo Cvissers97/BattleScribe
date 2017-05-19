8b7df040ede9b38200aa5eaf5730d2bdda2a770f
Justnuked <thommartens14@hotmail.com>
2017-05-19 13:18:23 +0200

    Detail screen

--- BattleScribe/Forms/DetailScreen.xaml.cs
+++ BattleScribe/Forms/DetailScreen.xaml.cs
@@ -75,14 +75,15 @@ namespace BattleScribe.Forms
             langs = new List<Language>();
             spells = new List<Spell>();
             charId = character.GetID();
-            c.SetSkillList(skills);
-            c.SetLangList(langs);
+
             
 
             c = db.GetCharacterById(charId);
             skills = db.GetSkillsByCharId(charId);
             langs = db.GetLangsByCharId(charId);
             spells = db.GetSpellsByCharId(charId);
+            c.SetSkillList(skills);
+            c.SetLangList(langs);
             Init();
 
         }
