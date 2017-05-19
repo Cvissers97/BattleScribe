d32ef16ad6945b37263d4e85506e4f8c7b7244fa
Jack // Casper Vissers <C.vissers97@outlook.com>
2017-05-19 14:55:07 +0200

    spell

---
+++ BattleScribe/Controls/Spells/SpellControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BattleScribe.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BattleScribe.Controls.Spells
+{
+    /// <summary>
+    /// Interaction logic for SpellControl.xaml
+    /// </summary>
+    public partial class SpellControl : UserControl
+    {
+        Spell spell;
+
+        public SpellControl()
+        {
+            InitializeComponent();
+        }
+
+        public SpellControl(Spell spell)
+        {
+            InitializeComponent();
+            this.spell = spell;
+        }
+    }
+}
